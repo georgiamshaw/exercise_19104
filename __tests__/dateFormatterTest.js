7be7cdb4f2642ab216bfe88d9278773e45185a34
georgia.shaw <georgia@cranbrookw4.com>
2019-10-28 23:49:48 +0000

    write function convertDateToCorrectFormat to change date into DD/MM/YYYY format and pass failing test

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -6,16 +6,16 @@ it("1 equals 1", () => {
   expect(one).toBe(1);
 });
 
-it('when the system date and the date to format are the same day formats as "TODAY"', () => {
+it('when system date and date to format are the same day, format as "TODAY"', () => {
   const December = 11; //js Date object month is indexed from 0
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
   expect(format(dateTimeToFormat, systemDateTime)).toBe("TODAY");
 });
 
-it("when the system date and the date to format are not the same day", () => {
-  const December = 11; //js Date object month is indexed from 0
-  const systemDateTime = new Date(2018, December, 16, 10, 5).getTime();
-  const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime();
-  expect(format(dateTimeToFormat, systemDateTime)).toBe("16/11/2018");
+it("when system date and date to format are not the same day, format system date as DD/MM/YYYY", () => {
+  const November = 10; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2018, November, 12, 11, 30).getTime();
+  const dateTimeToFormat = new Date(2017, November, 10, 11, 30).getTime();
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("10/11/2017");
 });
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,13 +1,23 @@
-const convertDate = () => {};
+const convertDateToCorrectFormat = date => {
+  // create a date object
+  const d = new Date(date);
+  // find the day, month and year of the date
+  let day = "" + d.getDate();
+  let month = "" + (d.getMonth() + 1);
+  let year = d.getFullYear();
+  return [day, month, year].join("/");
+};
 
 const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
   // turn inputs into date/time objects and then convert the date part of
   // the object into a string
-  const systemDateTime = new Date(dateToFormatTimeMillis).toDateString();
-  const dateTimeToFormat = new Date(systemDateTimeMillis).toDateString();
+  const systemDateTime = new Date(systemDateTimeMillis).toDateString();
+  const dateTimeToFormat = new Date(dateToFormatTimeMillis).toDateString();
   // if inputs are the same, return string saying "today", if not, return a
   // date in the format of DD/MM/YYYY
-  return systemDateTime === dateTimeToFormat ? "TODAY" : "";
+  return systemDateTime === dateTimeToFormat
+    ? "TODAY"
+    : convertDateToCorrectFormat(dateTimeToFormat);
 };
 
 module.exports = { format };

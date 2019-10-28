106e125f292464bab80b0cf00ecc83e97ecb11a2
georgia.shaw <georgia@cranbrookw4.com>
2019-10-28 23:09:55 +0000

    write a test to make sure test file works, and write enough code to pass test comparing the same dates

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,8 +1,14 @@
-import {format} from '../src/dateFormatter';
+import { format } from "../src/dateFormatter";
+
+// ensure the test file is working
+it("1 equals 1", () => {
+  const one = 1;
+  expect(one).toBe(1);
+});
 
 it('when the system date and the date to format are the same day formats as "TODAY"', () => {
   const December = 11; //js Date object month is indexed from 0
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
-  expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("TODAY");
 });
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,11 @@
-export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
+  // turn inputs into date/time objects and then convert the date part of
+  // the object into a string
+  const systemDateTime = new Date(dateToFormatTimeMillis).toDateString();
+  const dateTimeToFormat = new Date(systemDateTimeMillis).toDateString();
+  // if inputs are the same, return string saying "today", if not, return a
+  // date in the format of DD/MM/YYYY
+  return systemDateTime === dateTimeToFormat ? "TODAY" : "";
 };
+
+module.exports = { format };
